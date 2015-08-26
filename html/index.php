0a2a18d7cc9a6eefbb854cd2d894764f4466b588
Aayush Shaurya <aayush@cse.iitk.ac.in>
2015-08-26 20:16:28 +0530

    added few lines

---
+++ html/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>
+CS252 VM Landing Page
+</title>
+</head>
+<body>
+<div align="center">
+<h2>
+Welcome to 172.27.5.25
+</h2>
+<br>
+<a href="./Assignment%201/">Assignment 1</a>
+</div>
+</body>
+</html>
