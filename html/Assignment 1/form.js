8600658644722f48e2e22f7de8d32ad878347532
Aayush Shaurya <aayush@cse.iitk.ac.in>
2015-08-26 20:46:54 +0530

    asfadf

---
+++ html/Assignment 1/form.js
@@ -0,0 +1,5 @@
+if (document.getElementById('optionA').value == 'Create')
+document.getElementById('modal-body').innerHTML = '
+<form>
+Name:
+</form>';
--- html/Assignment 1/index.php
+++ html/Assignment 1/index.php
@@ -1,4 +1,23 @@
 <?php
 header('Content-type: application/json');
-echo $_REQUEST['q']." Entry";
+echo $_REQUEST['q']." Entry".'</h4> </div><div class="modal-body" align="left">';
+
+
+if($_REQUEST['q'] == 'Create')
+{
+	echo '
+	<form id="create-form" action="" method="post">
+	Name: <input type="text" name="NAME" value="Someone" required><br>
+	Date of Birth: <input type="text" name="DOB" value="2015-08-26"><br>  
+	Roll No. : <input type="text" name="ROLL" value="0" required><br>
+	Registration Number  :  <input type="text" name="REG" value="0" required><br>
+	CPI :  <input type="text" name="CPI" value="8" required>
+	</form>';
+}
+
+echo '</div>';
+
+
+
+
 ?>
